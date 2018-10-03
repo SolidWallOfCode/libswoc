addc1093ec6914eab261c019e7dff734ce58b034
Alan M. Carroll <amc@apache.org>
2018-10-03 12:15:19 -0500

    Update ATS revert script to be per module, starting with InstrusiveDList.

--- tools/ats-revert.sh
+++ tools/ats-revert.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+# Need to run this in the libswoc directory.
 
-for f in src/*.cc ; do
-  sed -iE --expr 's!swoc::!ts::!g' $f
-  sed -iE --expr 's!swoc/!ts/!' $f
-done
+# Do IntrusiveDList
+if cp include/swoc/IntrusiveDList.h ../ats/include/tscore ; then
+  sed -iE --expr 's!swoc::!ts::!g' ../ats/include/tscore/IntrusiveDList.h
+  sed -iE --expr 's!swoc/!ts/!' ../ats/include/tscore/IntrusiveDList.h
+  sed -iE --expr 's!namespace swoc/!namespace ts/!' ../ats/include/tscore/IntrusiveDList.h
+else
+  echo "Failed to copy IntrusiveDList.h"
+  exit 1;
+fi
