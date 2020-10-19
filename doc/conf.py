76ea4fa5e818687be77c2756cbe80d324586cd44
Alan M. Carroll <amc@apache.org>
2020-10-19 18:56:01 -0500

    Update to 1.2.12.

--- doc/conf.py
+++ doc/conf.py
@@ -80,7 +80,7 @@ project = u'Solid Wall Of C++'
 copyright = u'{}, amc@apache.org'.format(date.today().year)
 
 # The full version, including alpha/beta/rc tags.
-release = "1.2.11"
+release = "1.2.12"
 # The short X.Y version.
 version = '.'.join(release.split('.', 2)[:2])
 
