43294ea5626faf11be1a21f8cda23df0002c3c3b
Alan M. Carroll <amc@apache.org>
2020-10-19 18:56:01 -0500

    Update to 1.2.12.

--- code/include/swoc/swoc_version.h
+++ code/include/swoc/swoc_version.h
@@ -22,11 +22,11 @@
 #pragma once
 
 #if !defined(SWOC_VERSION_NS)
-#  define SWOC_VERSION_NS _1_2_11
+#  define SWOC_VERSION_NS _1_2_12
 #endif
 
 namespace swoc { inline namespace SWOC_VERSION_NS {
 static constexpr unsigned MAJOR_VERSION = 1;
 static constexpr unsigned MINOR_VERSION = 2;
-static constexpr unsigned POINT_VERSION = 11;
+static constexpr unsigned POINT_VERSION = 12;
 }} // namespace SWOC_VERSION_NS
