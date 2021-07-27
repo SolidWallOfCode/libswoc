486580d6a91ea2e000312ae9c4ac30006977c1ef
Alan M. Carroll <amc@apache.org>
2021-07-27 17:34:02 -0500

    Separate string_view utilities into separate files.

--- code/src/TextView.cc
+++ code/src/TextView.cc
@@ -15,47 +15,6 @@
 
 using namespace swoc::literals;
 
-int
-memcmp(std::string_view const &lhs, std::string_view const &rhs)
-{
-  int zret = 0;
-  size_t n = rhs.size();
-
-  // Seems a bit ugly but size comparisons must be done anyway to get the memcmp args.
-  if (lhs.size() < rhs.size()) {
-    zret = 1;
-    n    = lhs.size();
-  } else if (lhs.size() > rhs.size()) {
-    zret = -1;
-  } else if (lhs.data() == rhs.data()) { // same memory, obviously equal.
-    return 0;
-  }
-
-  int r = ::memcmp(lhs.data(), rhs.data(), n);
-  return r ? r : zret;
-}
-
-int
-strcasecmp(const std::string_view &lhs, const std::string_view &rhs)
-{
-  int zret = 0;
-  size_t n = rhs.size();
-
-  // Seems a bit ugly but size comparisons must be done anyway to get the @c strncasecmp args.
-  if (lhs.size() < rhs.size()) {
-    zret = 1;
-    n    = lhs.size();
-  } else if (lhs.size() > rhs.size()) {
-    zret = -1;
-  } else if (lhs.data() == rhs.data()) { // the same memory, obviously equal.
-    return 0;
-  }
-
-  int r = ::strncasecmp(lhs.data(), rhs.data(), n);
-
-  return r ? r : zret;
-}
-
 namespace swoc { inline namespace SWOC_VERSION_NS {
 
 /// @cond INTERNAL_DETAIL
