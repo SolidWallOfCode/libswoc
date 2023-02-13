0c74a982d7e8108438939328d6caafd1ea1f6796
Alan M. Carroll <amc@apache.org>
2023-02-13 14:04:49 -0600

    CMake: fix bad path in pc file.

--- code/libswoc.pc.cmake
+++ code/libswoc.pc.cmake
@@ -1,6 +1,6 @@
 prefix=@CMAKE_INSTALL_PREFIX@
 exec_prefix=${prefix}
-includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@/swoc
+includedir=${prefix}/@CMAKE_INSTALL_INCLUDEDIR@
 libdir=${exec_prefix}/@CMAKE_INSTALL_LIBDIR@
 
 Name: LibSWOC++
